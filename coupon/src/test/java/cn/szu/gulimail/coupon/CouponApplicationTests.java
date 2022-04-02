d8a59326859cd97e3730867fb30f30cb9918510b
chenyajun <554811048@qq.com>
2022-04-02 14:45:58 +0800

    项目结构搭建

---
+++ coupon/src/test/java/cn/szu/gulimail/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.szu.gulimail.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
